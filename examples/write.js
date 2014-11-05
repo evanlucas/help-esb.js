5eab59d88584c30d7f9b1bb644947d68f97cab89
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-11-05 09:52:02 -0600

    Don't do the timeout in the write example.
    This timeout was only there because of authentication not being required
    for sending messages before.

--- examples/write.js
+++ examples/write.js
@@ -1,10 +1,7 @@
 var HelpEsb = require('../help-esb');
 var client = new HelpEsb.Client(process.env.ESB_HOST, process.env.ESB_PORT);
 client.login('bar');
-
-setTimeout(function() {
-  client.send('asdf', {name: 'cool guy'});
-}, 2000);
+client.send('asdf', {name: 'cool guy'});
 
 client.on({type: 'error'}, function(err) {
   console.warn('Oh noes!');
